0a054b3740a055a7524f16d04acd6c26f389577b
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2024-10-09 18:24:43 +0000

    9bf3afa

--- PP2_1.py
+++ PP2_1.py
@@ -2,15 +2,27 @@
 
 def q1(): 
   #Write Assignment code here
+  num = int(input("In: "))
+  if num % 2 == 0:
+    print (f"{num} is even")
+
+  if num % 2 == 1:
+    print (f"{num} is odd")
 
 
 def q2(): 
   #Write Assignment code here
+  kalisz = input("In: ")
+  if kalisz == "Kalisz":
+    print ("teacher")
+  
+  if not kalisz == "Kalisz":
+    print("student")
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+# q1()
+# q2()
